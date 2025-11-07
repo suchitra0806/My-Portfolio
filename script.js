9dd5b62da556301c804403e70851bec017eaa30f
Suchitra Rajkumar <suchiraj0806@gmail.com>
2025-11-07 14:59:34 -0600

    removing old files and folders

--- script.js
+++
@@ -1,9 +0,0 @@
-const sidemenu = document.querySelector('#sidemenu');
-
-function open_menu() {
-    sidemenu.style.transform = 'translateX(-16rem)';
-}
-
-function close_menu() {
-    sidemenu.style.transform = 'translateX(16rem)';
-}
